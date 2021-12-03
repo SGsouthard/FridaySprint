83128451b2bcda883b5088b5a55c11c3ed0346b9
SGSouthard <sydneyaileensouthard@gmail.com>
2021-12-03 10:13:33 -0800

    i broke it, need to go in and reboot project

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const layouts = require('express-ejs-layouts');
 const PORT = process.env.PORT || 8000;
 
+const { Movie, Characters} = require('./models')
+const { Op } = require("sequelize");
+
 app.set('view engine', 'ejs'); // for our view (html like pages), we want to use 
 app.use(layouts);
 
@@ -16,7 +19,35 @@ app.get('/', function(req, res) {
 });
 ///----------------
 
-//
+// Movie.bulkCreate([
+//     { title: 'Snow White and the Seven Dwarfs', releaseDate: 1937, animated: true },
+//     { title: 'The Aristocats', releaseDate: 1970, animated: true },
+//     { title: 'Bedknobs and Broomsticks', releaseDate: 1971, animated: false },
+//     { title: 'Monsters, Inc', releaseDate: 2001, animated: true },
+//     { title: 'George of the Jungle', releaseDate: 1997, animated: false },
+// ]).then(function(newMovies){
+//     console.log('Bulk movies created', newMovies);
+// }).catch(function(error){
+//     console.log('Bulk movies not created', error);
+// });
+
+
+
+Characters.bulkCreate([
+    { name: 'Snow White', actor: 'Adriana Caselottie', movie: 'Snow White and the Seven Dwarfs' },
+    { name: 'Doc', actor: 'Roy Atwell', movie: 'Snow White and the Seven Dwarfs' },
+    { name: 'Grumpy', actor: 'Pinto Colvig', movie: 'Snow White and the Seven Dwarfs' },
+    { name: 'Sleepy', actor: 'Pinto Colvig', movie: 'Snow White and the Seven Dwarfs' },
+    { name: 'Happy', actor: 'Otis Harlan', movie: 'Snow White and the Seven Dwarfs' },
+    { name: 'Bashful', actor: 'Scotty Mattraw', movie: 'Snow White and the Seven Dwarfs' },
+    { name: 'Sneezy', actor: 'Billy Gilbert', movie: 'Snow White and the Seven Dwarfs' },
+    { name: 'Evil Queen', actor: 'Lucille La Verne', movie: 'Snow White and the Seven Dwarfs' },
+    { name: 'The Prince', actor: 'Harry Stockwell', movie: 'Snow White and the Seven Dwarfs' }
+]).then(function(newCharacters){
+    console.log('Bulk Characters created', newCharacters);
+}).catch(function(error){
+    console.log('Bulk Characters not created', error);
+});
 
 ///----------------
 app.listen(PORT, function() {
